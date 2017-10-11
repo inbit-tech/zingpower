6de5ba82bd45f8efc7a855cb42ebab1c46593c26
Xiaonan Cui <xiaonan@innbit.com>
2017-10-11 15:22:16 +0800

    Change default font style to Dosis for better display

--- resources/views/layout/nav/header.blade.php
+++ resources/views/layout/nav/header.blade.php
@@ -1,3 +1,25 @@
-<nav class="navbar sticky-top navbar-light bg-light">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
 	<a class="navbar-brand" href="#">Zing Power Header</a>
+	<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="/index">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Products</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Solutions</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/user-story">User Story</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">About Us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/contact">Contact Us</a>
+			</li>
+		</ul>
+	</div>
 </nav>

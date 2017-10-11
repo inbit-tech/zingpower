5b529bea997a6273394506ebc3b3e30266de846c
Xiaonan Cui <xiaonan@innbit.com>
2017-10-11 11:34:41 +0800

    Add routes for pages in sitemap

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,9 +6,69 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    // homepage
+    // Homepage
     public function index()
     {
     	return view('home');
+    }
+
+    // Products - Mobile Phone
+    public function mobilePhone()
+    {
+    	return view('');
+    }
+
+    // Products - Walkie Talkie
+    public function walkieTalkie()
+    {
+    	return view('');
+    }
+
+    // Products - Smart Room Controller
+    public function smartRoomController()
+    {
+    	return view('');
+    }
+
+    // Solutions - System Integration
+    public function sysIntegration()
+    {
+    	return view('');
+    }
+
+    // Solutions - Device Management System
+    public function dms()
+    {
+    	return view('');
+    }
+
+    // Solutions - Zigbee, Z-Wave or LoRaWAN Local Network Setup
+    public function lms()
+    {
+    	return view('');
+    }
+
+    // User Story
+    public function userStory()
+    {
+    	return view('');
+    }
+
+    // About Us - Zing Power
+    public function zingpower()
+    {
+    	return view('');
+    }
+
+    // About Us - News
+    public function news()
+    {
+    	return view('');
+    }
+
+    // Contact Us
+    public function contact()
+    {
+    	return view('');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,45 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| ZingPower.co Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+| zingpower.co sitemap for references
+| + Homepage
+| + Products
+|	- mobile phone
+|	- walkie talkie
+| 	- smart room controller
+| + Solutions
+|	- system integration
+|	- device management system [dms]
+| 	- Zigbee, Z-Wave or LoRaWAN Local Network Setup [lms]
+| + User Story
+| + About Us
+|	- Zing Power
+|	- News
+| + Contact Us
 */
 
-// Setup Site Routers
+// Router for Homepage
 Route::get('/', 'PagesController@index');
+
+// Router for Products
+Route::get('/products/mobile-phone', 'PagesController@mobilePhone');
+Route::get('/products/walkie-talkie', 'PagesController@walkieTalkie');
+Route::get('/products/smart-room-controller', 'PagesController@smartRoomController');
+
+// Router for Solutions
+Route::get('/solutions/system-integration', 'PagesController@sysIntegration');
+Route::get('/solutions/dms', 'PagesController@dms');
+Route::get('/solutions/lms', 'PagesController@lms');
+
+// Router for User Story
+Route::get('/user-story', 'PagesController@userStory');
+
+// Route for About Us
+Route::get('/about/zing-power', 'PagesController@zingpower');
+Route::get('/about/news', 'PagesController@news');
+
+// Route for Contact Us
+Route::get('/contact', 'PagesController@contact');

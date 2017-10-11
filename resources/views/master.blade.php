7f26c7485cdba22b331eb13f4cc4dcf436e717ba
Xiaonan Cui <xiaonan@innbit.com>
2017-10-11 14:11:42 +0800

    Create master template page for other pages to extend

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,1 +1,86 @@
-Homepage
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+    <meta name="author" content="">
+
+	<title>Zing Power</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+	<!-- Custom CSS -->
+	<style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+	<!-- Page Header Section -->
+	<nav class="navbar sticky-top navbar-light bg-light">
+		<a class="navbar-brand" href="#">Zing Power Header</a>
+	</nav>
+
+	<!-- Page Content Section -->
+	@yield ('content')
+
+	<!-- Page Footer Section -->
+	<nav class="navbar sticky-bottom navbar-light bg-light">
+		<a class="navbar-brand" href="#">Zing Power Footer</a>
+	</nav>
+
+	<!-- Optional JavaScript -->
+	<script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>

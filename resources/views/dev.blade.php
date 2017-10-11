14a7116c97000dbe7a87f7436615f103ef9f5d75
Xiaonan Cui <xiaonan@innbit.com>
2017-10-11 15:34:27 +0800

    Add Dev Notes for references

--- resources/views/dev.blade.php
+++ resources/views/dev.blade.php
@@ -3,9 +3,15 @@
 @section ('content')
 <div class="flex-center position-ref full-height">
     <div class="content">
-        <div class="title m-b-md">
-            v 0.2
-        </div>
+		<div class="col-md-auto">
+			<h3>Dev Notes :</h3>
+			<ul>
+				<li>route '/' - dev.blade.php</li>
+				<li>route '/index' - home/index.blade.php</li>
+				<li>master.blade.php is the master page</li>
+				<li>page partials locates in views/layout</li>
+			</ul>
+		</div>
     </div>
 </div>
 @endsection

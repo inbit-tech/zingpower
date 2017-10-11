958554225947e59ea45189da0ffb01e1946ad430
Xiaonan Cui <xiaonan@innbit.com>
2017-10-11 14:47:19 +0800

    Optimize master page - Move code to tpl folder files

---
+++ resources/views/layout/nav/header.blade.php
@@ -0,0 +1,3 @@
+<nav class="navbar sticky-top navbar-light bg-light">
+	<a class="navbar-brand" href="#">Zing Power Header</a>
+</nav>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,38 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<!-- meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-    <meta name="author" content="">
+<!-- Page Template Header Section -->
+@include ('layout.tpl.header')
 
-	<title>Zing Power</title>
+<!-- Page Header Section -->
+@include ('layout.nav.header')
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<!-- Page Content Section -->
+@yield ('content')
 
-	<!-- Google Fonts -->
-	<link href="https://fonts.cat.net/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<!-- Page Footer Section -->
+@include ('layout.nav.footer')
 
-	<!-- Custom CSS -->
-	<link href="{{ asset('/css/master.css') }}" rel="stylesheet" />
-</head>
-<body>
-	<!-- Page Header Section -->
-	<nav class="navbar sticky-top navbar-light bg-light">
-		<a class="navbar-brand" href="#">Zing Power Header</a>
-	</nav>
-
-	<!-- Page Content Section -->
-	@yield ('content')
-
-	<!-- Page Footer Section -->
-	<nav class="navbar sticky-bottom navbar-light bg-light">
-		<a class="navbar-brand" href="#">Zing Power Footer</a>
-	</nav>
-
-	<!-- Optional JavaScript -->
-	<script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-</body>
-</html>
+<!-- Page Template Footer Section -->
+@include ('layout.tpl.footer')

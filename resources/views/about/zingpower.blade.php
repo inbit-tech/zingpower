0c7972bf04468682ac119125a5a6f0ef3d47da0f
Xiaonan Cui <xiaonan@innbit.com>
2017-10-16 19:28:24 +0800

    Add HTML layout in About Us Page

--- resources/views/about/zingpower.blade.php
+++ resources/views/about/zingpower.blade.php
@@ -1,11 +1,42 @@
 @extends ('master')
 
 @section ('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            About Zing Power
-        </div>
-    </div>
+<!-- Header image include page title -->
+<div>
+	<h3>Know More About Us</h3>
 </div>
+
+<!-- Current Year Content -->
+<div>
+	<h4>2017</h3>
+	<h3>"Our greatest motivation in life comes from not knowing the future"</h3>
+</div>
+
+<!-- Featured image about zingpower -->
+<div>
+	<img src="" alt="">	
+</div>
+
+<!-- History Content -->
+<div>
+	<h4>Our History</h4>
+	<article>
+		At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+	</article>
+	<img src="" alt="">
+	<img src="" alt="">
+	<article>
+		At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+	</article>
+	<article>
+		At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+	</article>
+	<img src="" alt="">
+	<img src="" alt="">
+	<article>
+		At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+	</article>
+	<h4>2009</h4>
+</div>
+
 @endsection

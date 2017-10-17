bbb70ab6c7cb065f7b7242aa36d5e5774fc793f0
Xiaonan Cui <xiaonan@innbit.com>
2017-10-17 19:03:02 +0800

    Complete general layout for about page

--- resources/views/about/zingpower.blade.php
+++ resources/views/about/zingpower.blade.php
@@ -2,54 +2,77 @@
 
 @section ('content')
 <!-- Header image include page title -->
-<div>
-	<h3>Know More About Us</h3>
+<div class="jumbotron jumbotron-fluid">
+	<div class="container-fluid">
+		<h2 class="display-4 text-center">Know More About Us</h2>
+	</div>
 </div>
 
 <!-- Current Year Content -->
-<div>
-	<h4>2017</h3>
-	<h3>"Our greatest motivation in life comes from not knowing the future"</h3>
+<div class="container-fluid">
+	<h3 class="text-center">2017</h3>
+	<h3 class="text-center">"Our greatest motivation in life comes from not knowing the future"</h3>
 </div>
 
 <!-- Featured image about zingpower -->
-<div>
-	<img src="" alt="">	
+<div class="container-fluid text-center">
+	<img src="https://placeimg.com/1120/500/tech" class="img-fluid" alt="">
+</div>
+
+<div class="container-fluid">
+	<h4 class="display-4">Our History</h4>
 </div>
 
 <!-- History Content -->
-<div>
-	<h4>Our History</h4>
-
-	<div>
-		<article>
-			At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
-		</article>
-		<img src="" alt="">
+<div class="container-fluid">
+	<!-- Row 1 -->
+	<div class="row">
+		<div class="col">
+			<p>
+				At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+			</p>
+		</div>
+		<div class="col">
+			<img src="https://placeimg.com/450/200/any" alt="">
+		</div>
 	</div>
-
-	<div>
-		<img src="" alt="">
-		<article>
-			At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
-		</article>
+	<!-- Row 2 -->
+	<div class="row">
+		<div class="col">
+			<img src="https://placeimg.com/450/200/any" alt="">
+		</div>
+		<div class="col">
+			<p>
+				At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+			</p>
+		</div>
 	</div>
-
-	<div>
-		<article>
-			At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
-		</article>
-		<img src="" alt="">
+	<!-- Row 3 -->
+	<div class="row">
+		<div class="col">
+			<p>
+				At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+			</p>
+		</div>
+		<div class="col">
+			<img src="https://placeimg.com/450/200/any" alt="">
+		</div>
 	</div>
-
-	<div>
-		<img src="" alt="">
-		<article>
-			At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
-		</article>
+	<!-- Row 4 -->
+	<div class="row">
+		<div class="col">
+			<img src="https://placeimg.com/450/200/any" alt="">
+		</div>
+		<div class="col">
+			<p>
+				At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+			</p>
+		</div>
 	</div>
+</div>
 
-	<h4>To Be Continued</h4>
+<div class="container-fluid">
+	<h4 class="display-4">To Be Continued</h4>
 </div>
 
 @endsection

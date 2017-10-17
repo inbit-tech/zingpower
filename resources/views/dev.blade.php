bb9249de92b0e5f933faa0e3c5c6d3f8cdbf725e
Xiaonan Cui <xiaonan@innbit.com>
2017-10-18 00:05:00 +0800

    Restore master branch n Add new style dev-master page info

--- resources/views/dev.blade.php
+++ resources/views/dev.blade.php
@@ -1,17 +1,30 @@
 @extends ('master')
 
 @section ('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-		<div class="col-md-auto">
-			<h3>Dev Notes :</h3>
+<div class="cols-12" style="height: 90%">
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			<h1 class="display-3">Dev Notes</h1>
+			<p class="lead">This is the page for develop purpose only, which includes bugs and notes</p>
+		</div>
+	</div>
+
+	<div class="container-fluid row" style="padding: 0 80px">
+		<div class="col" style="color: red">
+			<h4>Bugs : </h4>
 			<ul>
-				<li>route '/' - dev.blade.php</li>
-				<li>route '/index' - home/index.blade.php</li>
-				<li>master.blade.php is the master page</li>
-				<li>page partials locates in views/layout</li>
+				<li>Testing workflow</li>
 			</ul>
 		</div>
-    </div>
+
+		<div class="col" style="color: blue">
+			<h4>Notes : </h4>
+			<ul>
+				<li>Delete 'Master-Page' Branch</li>
+				<li>Restore 'Master' Branch</li>
+				<li>app.css - master.css - boostrap - custom.css</li>
+			</ul>
+		</div>
+	</div>
 </div>
 @endsection

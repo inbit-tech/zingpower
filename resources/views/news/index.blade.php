e0ea8ca1717de94cb5067155330e62ce870001bb
Xiaonan Cui <xiaonan@innbit.com>
2017-10-18 19:04:54 +0800

    Prepare news page dev - title n css file setup

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,5 +1,15 @@
 @extends ('master')
 
+<!-- Page Title -->
+@section ('page-title')
+<title>News - Zing Power</title>
+@endsection
+
+<!-- Custom CSS for News Page -->
+@section ('custom-css')
+<link href="{{ asset('/css/pages/news.css') }}" rel="stylesheet">
+@endsection
+
 @section ('content')
 <h1>News</h1>
 @endsection

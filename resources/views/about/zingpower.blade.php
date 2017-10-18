39ff2d85b7d79e8cd8b7d9e13a8b8e3ef08cd6f7
Xiaonan Cui <xiaonan@innbit.com>
2017-10-18 18:30:45 +0800

    Complete final style of about page timeline

--- resources/views/about/zingpower.blade.php
+++ resources/views/about/zingpower.blade.php
@@ -18,9 +18,6 @@
 	</div>
 </div>
 
-<!-- Timeline -->
-<div class="timeline"></div>
-
 <!-- Current Year Content -->
 <div id="about-header" class="container-fluid text-center">
 	<!-- Featured Title -->
@@ -37,8 +34,94 @@
 	<div class="container-fluid hero-img"></div>
 </div>
 
-<!-- History Content -->
+<!-- Zing Power History -->
+<section id="timeline">
+	<div class="time-card-wrapper">
+		<!-- Year 2016 -->
+		<div class="time-card time-card--1">
+			<div class="head">
+				<div class="number-box">
+					<span>2016</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Text</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="">
+			</div>
+		</div>
 
+		<!-- Year 2015 -->
+		<div class="time-card time-card--2">
+			<div class="head">
+				<div class="number-box">
+					<span>2015</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Text</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="">
+			</div>
+		</div>
+
+		<!-- Year 2014 -->
+		<div class="time-card time-card--3">
+			<div class="head">
+				<div class="number-box">
+					<span>2014</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Text</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="">
+			</div>
+		</div>
+
+		<!-- Year 2013 -->
+		<div class="time-card time-card--4">
+			<div class="head">
+				<div class="number-box">
+					<span>2013</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Text</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="">
+			</div>
+		</div>
+
+		<!-- Year 2012 -->
+		<div class="time-card time-card--5">
+			<div class="head">
+				<div class="number-box">
+					<span>2012</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Text</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="">
+			</div>
+		</div>
+
+		<!-- Year 2011 -->
+		<div class="time-card time-card--6">
+			<div class="head">
+				<div class="number-box">
+					<span>2011</span>
+				</div>
+				<h2><span class="small">Subtitle</span> Text</h2>
+			</div>
+			<div class="body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p>
+				<img src="http://placehold.it/1000x500" alt="">
+			</div>
+		</div>
+	</div>
+</section>
 
 <div class="container-fluid text-center">
 	<h4 class="display-4" id="found-year">2009</h4>

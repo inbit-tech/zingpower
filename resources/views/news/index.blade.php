5f823d7f3d4171855f2ea1b2ffa260e7894ca246
Xiaonan Cui <xiaonan@innbit.com>
2017-10-18 19:54:05 +0800

    Complete carousel

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -11,5 +11,40 @@
 @endsection
 
 @section ('content')
-<h1>News</h1>
+<!-- Carousel -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img class="d-block w-100" src="https://placeimg.com/1280/380/tech" alt="">
+			<div class="carousel-caption d-none d-md-block">
+				<h2>Slide 1</h2>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="https://placeimg.com/1280/380/people" alt="">
+			<div class="carousel-caption d-none d-md-block">
+				<h2>Slide 2</h2>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="https://placeimg.com/1280/380/nature" alt="">
+			<div class="carousel-caption d-none d-md-block">
+				<h2>Slide 3</h2>
+			</div>
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
 @endsection

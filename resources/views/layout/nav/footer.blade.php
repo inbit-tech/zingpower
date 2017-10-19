0ed421b5becb9a0ff6d6453a9fd6a1c373b33a8b
vayne <yaoxi@innbit.com>
2017-10-19 21:58:59 +0800

    init footer section

--- resources/views/layout/nav/footer.blade.php
+++ resources/views/layout/nav/footer.blade.php
@@ -1,3 +1,27 @@
-<nav class="navbar sticky-bottom navbar-light bg-light">
-	<a class="navbar-brand" href="#">Zing Power Footer</a>
+<nav class="navbar sticky-bottom navbar-light bg-light flex-space-a">
+    <div class="footer-flex">
+        <h3><strong>Products</strong></h3>
+        <ul>
+            <li><a href="" title="">Moblie Phone</a></li>
+            <li><a href="" title="">Walkie Takie</a></li>
+            <li><a href="" title="">Wireless Charger</a></li>
+            <li><a href="" title="">Smart Room Control</a></li>
+        </ul>
+    </div>
+    <div class="footer-flex">
+        <h3><strong>Solutions</strong></h3>
+        <ul>
+            <li><a href="" title="">System Integration</a></li>
+            <li><a href="" title="">Device Management System</a></li>
+            <li><a href="" title="">Zigbee,Z-Wave or LoRaWAN</a></li>
+        </ul>
+    </div>
+    <div class="floow">
+        <h3><strong>Follow Us</strong></h3>
+        <ul>
+            <li><a href="" title=""><img src=""></a></li>
+            <li><a href="" title=""><img src=""></a></li>
+            <li><a href="" title=""><img src=""></a></li>
+        </ul>
+    </div>
 </nav>

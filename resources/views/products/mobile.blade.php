df557c1f3ab0a505bbfec0ce4a6c6e02924b0589
Xiaonan Cui <xiaonan@innbit.com>
2017-10-20 02:56:34 +0800

    Complete products sample pages

--- resources/views/products/mobile.blade.php
+++ resources/views/products/mobile.blade.php
@@ -1,5 +1,24 @@
 @extends ('master')
 
+<!-- Page Title -->
+@section ('page-title')
+<title>Mobile Phone - Zing Power</title>
+@endsection
+
+<!-- Custom CSS for Mobile Phone Product Page -->
+@section ('custom-css')
+<link href="{{ asset('/css/pages/mobile.css') }}" rel="stylesheet">
+@endsection
+
 @section ('content')
-<h1>Mobile Phone</h1>
+<div class="site-wrapper">
+	<div class="site-wrapper-inner">
+		<div class="cover-container">
+			<div class="inner cover">
+				<h1 class="cover-heading">Product - Mobile Phone</h1>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
--- resources/views/products/wata.blade.php
+++ resources/views/products/wata.blade.php
@@ -1,5 +1,24 @@
 @extends ('master')
 
+<!-- Page Title -->
+@section ('page-title')
+<title>Walkie Talkie - Zing Power</title>
+@endsection
+
+<!-- Custom CSS for Mobile Phone Product Page -->
+@section ('custom-css')
+<link href="{{ asset('/css/pages/wata.css') }}" rel="stylesheet">
+@endsection
+
 @section ('content')
-<h1>Walkie Talkie</h1>
+<div class="site-wrapper">
+	<div class="site-wrapper-inner">
+		<div class="cover-container">
+			<div class="inner cover">
+				<h1 class="cover-heading">Product - Walkie Talkie</h1>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection

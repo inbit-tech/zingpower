7265d36d014cda806997deb08dd345d0337d3374
Xiaonan Cui <xiaonan@innbit.com>
2017-10-20 01:57:12 +0800

    Prepare for news info page

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,10 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<h1>{{ $zp_news->title }}</h1>
-</body>
-</html>
+@extends ('master')
+
+<!-- Page Title -->
+@section ('page-title')
+<title>{{ $zp_news->title }} - News - Zing Power</title>
+@endsection
+
+<!-- Custom CSS for News Article -->
+@section ('custom-css')
+<link href="{{ asset('/css/pages/news-show.css') }}" rel="stylesheet">
+@endsection
+
+@section ('content')
+<h1>{{ $zp_news->title }}</h1>
+@endsection

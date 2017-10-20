da1bee8ab728d614eb7d7aac80180e4388d4a8f8
vayne <yaoxi@innbit.com>
2017-10-20 15:40:34 +0800

    complete footer

--- resources/views/layout/nav/footer.blade.php
+++ resources/views/layout/nav/footer.blade.php
@@ -1,27 +1,33 @@
-<nav class="navbar sticky-bottom navbar-light bg-light flex-space-a">
-    <div class="footer-flex">
-        <h3><strong>Products</strong></h3>
-        <ul>
+<nav class="navbar sticky-bottom navbar-light bg-light flex-space-a d-flex justify-content-around align-items-start">
+    <div class="footer-list">
+        <h3>Products</h3>
+        <ul class="nav flex-column">
             <li><a href="" title="">Moblie Phone</a></li>
             <li><a href="" title="">Walkie Takie</a></li>
             <li><a href="" title="">Wireless Charger</a></li>
             <li><a href="" title="">Smart Room Control</a></li>
         </ul>
     </div>
-    <div class="footer-flex">
-        <h3><strong>Solutions</strong></h3>
-        <ul>
+    <div class="footer-list">
+        <h3>Solutions</h3>
+        <ul class="nav flex-column">
             <li><a href="" title="">System Integration</a></li>
             <li><a href="" title="">Device Management System</a></li>
             <li><a href="" title="">Zigbee,Z-Wave or LoRaWAN</a></li>
         </ul>
     </div>
-    <div class="floow">
-        <h3><strong>Follow Us</strong></h3>
-        <ul>
-            <li><a href="" title=""><img src=""></a></li>
-            <li><a href="" title=""><img src=""></a></li>
-            <li><a href="" title=""><img src=""></a></li>
+    <div class="footer-media">
+        <h3>Follow Us</h3>
+        <ul class="nav">
+            <li class="nav-item">
+                <a  class="nav-link" href="" title=""><img src="http://via.digital.com/50x50"></a>
+            </li>
+            <li class="nav-item">
+                <a  class="nav-link" href="" title=""><img src="http://via.digital.com/50x50"></a>
+            </li>
+            <li class="nav-item">
+                <a  class="nav-link" href="" title=""><img src="http://via.digital.com/50x50"></a>
+            </li>
         </ul>
     </div>
 </nav>

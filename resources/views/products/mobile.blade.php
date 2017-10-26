8e6c2353225bb11ad07a6553b6c523400198f6ab
xiaonan <xiaonan@innbit.com>
2017-10-26 12:33:42 +0800

    Remove css conflicts in Products n Solutions

--- resources/views/products/mobile.blade.php
+++ resources/views/products/mobile.blade.php
@@ -11,14 +11,5 @@
 @endsection
 
 @section ('content')
-<div class="site-wrapper">
-	<div class="site-wrapper-inner">
-		<div class="cover-container">
-			<div class="inner cover">
-				<h1 class="cover-heading">Product - Mobile Phone</h1>
-				</p>
-			</div>
-		</div>
-	</div>
-</div>
+<h1>Mobile Phone</h1>
 @endsection

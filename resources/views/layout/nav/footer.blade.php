74cfff2fa4ae7b050ef01748b1e910f5b95b9ad7
vayne <yaoxi@innbit.com>
2017-10-27 15:17:10 +0800

    footer section add copyright and optimization homepage css(font-size,remove footer.css code to master.css)

--- resources/views/layout/nav/footer.blade.php
+++ resources/views/layout/nav/footer.blade.php
@@ -1,30 +1,35 @@
-<nav class="navbar sticky-bottom navbar-light bg-light d-flex justify-content-around align-items-start">
-    <div class="text-align-sm">
-        <h3>Products</h3>
-        <ul class="nav flex-column">
-            <li><a href="" title="">Moblie Phone</a></li>
-            <li><a href="" title="">Walkie Takie</a></li>
-            <li><a href="" title="">Wireless Charger</a></li>
-            <li><a href="" title="">Smart Room Control</a></li>
-        </ul>
+<nav class="navbar sticky-bottom navbar-light bg-light d-flex flex-column align-items-center" id="footer">
+    <div class="w-100 d-flex flex-wrap justify-content-around align-items-start">
+        <div class="text-align-sm">
+            <h3>Products</h3>
+            <ul class="nav flex-column">
+                <li><a href="" title="">Moblie Phone</a></li>
+                <li><a href="" title="">Walkie Takie</a></li>
+                <li><a href="" title="">Wireless Charger</a></li>
+                <li><a href="" title="">Smart Room Control</a></li>
+            </ul>
+        </div>
+        <div class="text-align-sm">
+            <h3>Solutions</h3>
+            <ul class="nav flex-column">
+                <li><a href="" title="">System Integration</a></li>
+                <li><a href="" title="">Device Management System</a></li>
+                <li><a href="" title="">Zigbee,Z-Wave or LoRaWAN</a></li>
+            </ul>
+        </div>
+        <div class="text-align-sm">
+            <h3>Follow Us</h3>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a  class="nav-link" href="" title="facebook"><img src="http://via.digital.com/50x50"></a>
+                </li>
+                <li class="nav-item">
+                    <a  class="nav-link" href="//twitter.com/ZingPowerCo" title="twitter"><img src="http://via.digital.com/50x50"></a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div class="text-align-sm">
-        <h3>Solutions</h3>
-        <ul class="nav flex-column">
-            <li><a href="" title="">System Integration</a></li>
-            <li><a href="" title="">Device Management System</a></li>
-            <li><a href="" title="">Zigbee,Z-Wave or LoRaWAN</a></li>
-        </ul>
-    </div>
-    <div class="text-align-sm">
-        <h3>Follow Us</h3>
-        <ul class="nav">
-            <li class="nav-item">
-                <a  class="nav-link" href="" title="facebook"><img src="http://via.digital.com/50x50"></a>
-            </li>
-            <li class="nav-item">
-                <a  class="nav-link" href="//twitter.com/ZingPowerCo" title="twitter"><img src="http://via.digital.com/50x50"></a>
-            </li>
-        </ul>
+    <div class="w-100 text-center bg-secondary text-white ">
+        <p>copyright@2017</p>
     </div>
 </nav>

5e6b35411869ff9ca23bff41038629c8d2f23a60
vayne <yaoxi@innbit.com>
2017-10-28 01:18:36 +0800

    some notes modify

--- resources/views/products/wata.blade.php
+++ resources/views/products/wata.blade.php
@@ -5,7 +5,7 @@
 <title>Walkie Talkie - Zing Power</title>
 @endsection
 
-<!-- Custom CSS for Mobile Phone Product Page -->
+<!-- Custom CSS for walkie talkie Product Page -->
 @section ('custom-css')
 <link href="{{ asset('/css/pages/wata.css') }}" rel="stylesheet">
 @endsection
--- resources/views/products/wireless.blade.php
+++ resources/views/products/wireless.blade.php
@@ -5,7 +5,7 @@
 <title>Wireless Charger - Zing Power</title>
 @endsection
 
-<!-- Custom CSS for Mobile Phone Product Page -->
+<!-- Custom CSS for wireless charger Product Page -->
 @section ('custom-css')
 <link href="{{ asset('/css/pages/wireless.css') }}" rel="stylesheet">
 @endsection

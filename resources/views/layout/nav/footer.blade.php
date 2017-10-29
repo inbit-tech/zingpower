44cc1c5129e533fb127ee0d7cb02dbfd3dc7c5ef
xiaonan <xiaonan@innbit.com>
2017-10-30 06:01:44 +0800

    Update style for footer section

--- resources/views/layout/nav/footer.blade.php
+++ resources/views/layout/nav/footer.blade.php
@@ -1,35 +1,49 @@
 <nav class="navbar sticky-bottom navbar-light bg-light d-flex flex-column align-items-center" id="footer-wrapper">
-    <div class="w-100 d-flex flex-wrap justify-content-around align-items-start footer">
-        <div class="text-align-sm products">
-            <h3>Products</h3>
-            <ul class="nav flex-column">
-                <li><a href="" title="">Moblie Phone</a></li>
-                <li><a href="" title="">Walkie Takie</a></li>
-                <li><a href="" title="">Wireless Charger</a></li>
-                <li><a href="" title="">Smart Room Control</a></li>
-            </ul>
-        </div>
-        <div class="text-align-sm solutions">
-            <h3>Solutions</h3>
-            <ul class="nav flex-column">
-                <li><a href="" title="">System Integration</a></li>
-                <li><a href="" title="">Device Management System</a></li>
-                <li><a href="" title="">Zigbee,Z-Wave or LoRaWAN</a></li>
-            </ul>
-        </div>
-        <div class="text-align-sm follow-us">
-            <h3>Follow Us</h3>
-            <ul class="nav">
-                <li class="nav-item">
-                    <a  class="nav-link" href="" title="facebook"><img src="http://via.digital.com/50x50"></a>
-                </li>
-                <li class="nav-item">
-                    <a  class="nav-link" href="//twitter.com/ZingPowerCo" title="twitter"><img src="http://via.digital.com/50x50"></a>
-                </li>
-            </ul>
+    <div class="footer">
+        <div class="row">
+            <div class="col-4 products">
+                <h4>Products</h4>
+                <ul>
+                    <li>
+                        <a href="/products/mobile-phone">Mobile Phone</a>
+                    </li>
+                    <li>
+                        <a href="/products/walkie-talkie">Walkie Talkie</a>
+                    </li>
+                    <li>
+                        <a href="/products/wirelss-charger">Wireless Charger</a>
+                    </li>
+                    <li>
+                        <a href="/products/smart-room-controller">Smart Room Controller</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-4 solutions">
+                <h4>Solutions</h4>
+                <ul>
+                    <li>
+                        <a href="/solutions/system-integration">Systeim Integration</a>
+                    </li>
+                    <li>
+                        <a href="/solutions/dms">Device Management System</a>
+                    </li>
+                    <li>
+                        <a href="/solutions/lns">Zigbee Z-Wave of LoRaWan</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-4 follow-us">
+                <h4>Follow Us</h4>
+                <a href="//www.facebook.com/Zing-Power-218782381993140">
+                    <img src=" {{ asset('/img/facebook.png') }} " alt="">
+                </a>
+                <a href="//twitter.com/ZingPowerCo">
+                    <img src=" {{ asset('/img/twitter.png') }} " alt="">
+                </a>
+            </div>
         </div>
     </div>
-    <div id="copyright" class="w-100 text-center bg-secondary text-white row">
-        <div class="col align-self-center">copyright &copy; 2017</div>
+    <div id="copyright" class="w-100 text-center bg-secondary text-white">
+        <p>copyright &copy; 2017</p>
     </div>
 </nav>

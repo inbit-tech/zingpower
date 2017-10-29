e49fa86cbbba7ce68f46b6800c21cb8f21320fbc
xiaonan <xiaonan@innbit.com>
2017-10-30 03:49:41 +0800

    Enhance header menu

--- public/js/master.js
+++ public/js/master.js
@@ -2,8 +2,14 @@ $(window).bind('scroll', function() {
 	if ($(window).scrollTop() > 50) {
 		$('.navbar').removeClass('navbar-dark');
 		$('.navbar').addClass('bg-light navbar-light');
+		$('#zp-logo').attr({
+			'src': '/img/zp-logo-color.png'
+		});
 	} else {
 		$('.navbar').removeClass('bg-light navbar-light');
 		$('.navbar').addClass('navbar-dark');
+		$('#zp-logo').attr({
+			'src': '/img/zp-logo-white.png'
+		});
 	}
 });

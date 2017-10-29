c869a397b0c39011473ea062f84642e08474aea3
xiaonan <xiaonan@innbit.com>
2017-10-30 00:31:26 +0800

    Add button when menu responsive

--- resources/views/layout/nav/header.blade.php
+++ resources/views/layout/nav/header.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
 	<a class="navbar-brand" href="/">Zing Power Header</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 	<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
 		<ul class="navbar-nav">
 			<li class="nav-item dropdown">

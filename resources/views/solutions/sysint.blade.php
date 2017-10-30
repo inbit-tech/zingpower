594fe0ea2d574c023166cc92f1caeeac8a06b655
xiaonan <xiaonan@innbit.com>
2017-10-31 01:11:21 +0800

    Complete page system integration

--- resources/views/solutions/sysint.blade.php
+++ resources/views/solutions/sysint.blade.php
@@ -18,43 +18,43 @@
 
 <!-- Content Image -->
 <section id="content-wrapper">
-	<div class="sysint-text">
+	<div class="sysint-text mx-auto">
 		<p class="sysint-intro">We deliver a complete and comprehensive working solution to help customer to achieve seamless operations and excellence in their services, cost reduction and simultaneously drive their revenue growth.</p>
 	</div>
-	<div class="sysint-func">
+	<div class="sysint-func-wrapper">
 		<!-- Function 1 -->
-		<div class="sysint-func-1">
-			<img src="http://placeimg.com/150/150/any" alt="">
-			<p class="sysint-func-1-title">Building</p>
-		</div>
+		<figure class="figure">
+		  <img src="http://placeimg.com/150/150/any" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+		  <figcaption class="figure-caption">Building</figcaption>
+		</figure>
 		<!-- Function 2 -->
-		<div class="sysint-func-2">
-			<img src="http://placeimg.com/150/150/any" alt="">
-			<p class="sysint-func-2-title">Hotel</p>
-		</div>
+		<figure class="figure">
+		  <img src="http://placeimg.com/150/150/any" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+		  <figcaption class="figure-caption">Hotel</figcaption>
+		</figure>
 		<!-- Function 3 -->
-		<div class="sysint-func-3">
-			<img src="http://placeimg.com/150/150/any" alt="">
-			<p class="sysint-func-3-title">School</p>
-		</div>
+		<figure class="figure">
+		  <img src="http://placeimg.com/150/150/any" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+		  <figcaption class="figure-caption">School</figcaption>
+		</figure>
 		<!-- Function 4 -->
-		<div class="sysint-func-4">
-			<img src="http://placeimg.com/150/150/any" alt="">
-			<p class="sysint-func-4-title">Hospital</p>
-		</div>
+		<figure class="figure">
+		  <img src="http://placeimg.com/150/150/any" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+		  <figcaption class="figure-caption">Hospital</figcaption>
+		</figure>
 		<!-- Function 5 -->
-		<div class="sysint-func-5">
-			<img src="http://placeimg.com/150/150/any" alt="">
-			<p class="sysint-func-5-title">Breeding</p>
-		</div>
+		<figure class="figure">
+		  <img src="http://placeimg.com/150/150/any" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
+		  <figcaption class="figure-caption">Breeding</figcaption>
+		</figure>
 	</div>
 	<div class="sysint-img">
-		<img src="http://placeimg.com/1280/450/any" alt="">
+		<div class="sysint-coverimg"></div>
 	</div>
-	<div class="sysint-card">
+	<div class="sysint-card-wrapper">
 		<!-- Card 1 -->
 		<div class="card" style="width: 20rem;">
-		  <img class="card-img-top" src="..." alt="Card image cap">
+		  <img class="card-img-top" src="http://placeimg.com/238/180/any" alt="Card image cap">
 		  <div class="card-body">
 		    <h4 class="card-title">Card title</h4>
 		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -63,7 +63,7 @@
 		</div>
 		<!-- Card 2 -->
 		<div class="card" style="width: 20rem;">
-		  <img class="card-img-top" src="..." alt="Card image cap">
+		  <img class="card-img-top" src="http://placeimg.com/238/180/any" alt="Card image cap">
 		  <div class="card-body">
 		    <h4 class="card-title">Card title</h4>
 		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -72,7 +72,7 @@
 		</div>
 		<!-- Card 3 -->
 		<div class="card" style="width: 20rem;">
-		  <img class="card-img-top" src="..." alt="Card image cap">
+		  <img class="card-img-top" src="http://placeimg.com/238/180/any" alt="Card image cap">
 		  <div class="card-body">
 		    <h4 class="card-title">Card title</h4>
 		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -81,7 +81,7 @@
 		</div>
 		<!-- Card 4 -->
 		<div class="card" style="width: 20rem;">
-		  <img class="card-img-top" src="..." alt="Card image cap">
+		  <img class="card-img-top" src="http://placeimg.com/238/180/any" alt="Card image cap">
 		  <div class="card-body">
 		    <h4 class="card-title">Card title</h4>
 		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

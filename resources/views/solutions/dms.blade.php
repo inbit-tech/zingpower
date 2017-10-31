88ab8ad6d3437410c78c1c784031ce3ed1bd2eb9
Xiaonan Cui <xiaonan@innbit.com>
2017-10-31 22:33:51 +0800

    Fill table content

--- resources/views/solutions/dms.blade.php
+++ resources/views/solutions/dms.blade.php
@@ -41,91 +41,91 @@
 	<div class="dms-func-img-wrapper">
 		<div class="dms-func-img"></div>
 	</div>
-	<div>
+	<div class="table-title">
 		<p>Why Choose Our Solution</p>
 	</div>
-	<table class="table mx-auto">
+	<table class="table mx-auto border">
 	  	<thead>
 		    <tr>
-				<th scope="col">Items</th>
-				<th scope="col">Traditional Smart Solution</th>
-				<th scope="col">Our Smart Solution</th>
+				<th class="border bg-light text-dark" scope="col">Items</th>
+				<th class="border bg-secondary text-white" scope="col">Traditional Smart Solution</th>
+				<th class="border bg-primary text-white" scope="col">Our Smart Solution</th>
 		    </tr>
 	  	</thead>
 	  	<tbody>
-	  		<tr>
-	  			<td colspan="3">Hardware</td>
+	  		<tr class="table-row-header">
+	  			<td class="border" colspan="3">Hardware</td>
 	  		</tr>
 			<tr>
-				<td>Cabling</td>
-				<td>KNS or CAT6E type of cable through out the property</td>
-				<td>None</td>
+				<td class="border bg-light text-dark">Cabling</td>
+				<td class="border bg-secondary text-white">KNS or CAT6E type of cable through out the property</td>
+				<td class="border bg-primary text-white">None</td>
 			</tr>
 			<tr>
-				<td>Gateway Modules</td>
-				<td>Yes</td>
-				<td>None</td>
+				<td class="border bg-light text-dark">Gateway Modules</td>
+				<td class="border bg-secondary text-white">Yes</td>
+				<td class="border bg-primary text-white">None</td>
 			</tr>
 			<tr>
-				<td>Pad/Mobile</td>
-				<td>Yes</td>
-				<td>Yes</td>
+				<td class="border bg-light text-dark">Pad/Mobile</td>
+				<td class="border bg-secondary text-white">Yes</td>
+				<td class="border bg-primary text-white">Yes</td>
 			</tr>
 			<tr>
-				<td>Execution Modules</td>
-				<td>Yes</td>
-				<td>None</td>
+				<td class="border bg-light text-dark">Execution Modules</td>
+				<td class="border bg-secondary text-white">Yes</td>
+				<td class="border bg-primary text-white">None</td>
 			</tr>
 			<tr>
-				<td>Wall panels</td>
-				<td>Yes</td>
-				<td>None</td>
+				<td class="border bg-light text-dark">Wall panels</td>
+				<td class="border bg-secondary text-white">Yes</td>
+				<td class="border bg-primary text-white">None</td>
 			</tr>
 			<tr>
-				<td>Power distribution box</td>
-				<td>Yes</td>
-				<td>None</td>
+				<td class="border bg-light text-dark">Power distribution box</td>
+				<td class="border bg-secondary text-white">Yes</td>
+				<td class="border bg-primary text-white">None</td>
 			</tr>
-			<tr>
-	  			<td colspan="3">System</td>
+			<tr class="table-row-header">
+	  			<td class="border" colspan="3">System</td>
 	  		</tr>
 	  		<tr>
-				<td>Device Management System</td>
-				<td>None</td>
-				<td>Yes, customized</td>
+				<td class="border bg-light text-dark">Device Management System</td>
+				<td class="border bg-secondary text-white">None</td>
+				<td class="border bg-primary text-white">Yes, customized</td>
 			</tr>
 			<tr>
-				<td>Hardware management</td>
-				<td>Per item basis</td>
-				<td>All device managed by DMS</td>
+				<td class="border bg-light text-dark">Hardware management</td>
+				<td class="border bg-secondary text-white">Per item basis</td>
+				<td class="border bg-primary text-white">All device managed by DMS</td>
 			</tr>
 			<tr>
-				<td>Control APP</td>
-				<td>Yes, standard</td>
-				<td>Yes, customized</td>
+				<td class="border bg-light text-dark">Control APP</td>
+				<td class="border bg-secondary text-white">Yes, standard</td>
+				<td class="border bg-primary text-white">Yes, customized</td>
 			</tr>
 			<tr>
-				<td>Customization Level</td>
-				<td>Almost none, key components has to be KNS, Crestron or Control4 authorized products</td>
-				<td>Full customization from hardware to software</td>
+				<td class="border bg-light text-dark">Customization Level</td>
+				<td class="border bg-secondary text-white">Almost none, key components has to be KNS, Crestron or Control4 authorized products</td>
+				<td class="border bg-primary text-white">Full customization from hardware to software</td>
 			</tr>
-			<tr>
-	  			<td colspan="3">Installation & Maintenance</td>
+			<tr class="table-row-header">
+	  			<td class="border" colspan="3">Installation & Maintenance</td>
 	  		</tr>
 	  		<tr>
-				<td>Room down time</td>
-				<td>2 to 3 months depends on hotel size</td>
-				<td>None</td>
+				<td class="border bg-light text-dark">Room down time</td>
+				<td class="border bg-secondary text-white">2 to 3 months depends on hotel size</td>
+				<td class="border bg-primary text-white">None</td>
 			</tr>
 			<tr>
-				<td>Hardware maintenance</td>
-				<td>Complicated hardware system requires team of professionals to manage the maintenance</td>
-				<td>Phone, replace when fault and its charged automatically</td>
+				<td class="border bg-light text-dark">Hardware maintenance</td>
+				<td class="border bg-secondary text-white">Complicated hardware system requires team of professionals to manage the maintenance</td>
+				<td class="border bg-primary text-white">Phone, replace when fault and its charged automatically</td>
 			</tr>
 			<tr>
-				<td>Adaptability</td>
-				<td>Prefer new hotel at construction time</td>
-				<td>Either new or old hotel, and at any stage</td>
+				<td class="border bg-light text-dark">Adaptability</td>
+				<td class="border bg-secondary text-white">Prefer new hotel at construction time</td>
+				<td class="border bg-primary text-white">Either new or old hotel, and at any stage</td>
 			</tr>
 	  </tbody>
 	</table>

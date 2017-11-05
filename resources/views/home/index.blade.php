0a2fce49930caf5013c2e2b34ea9d80f8ee68f67
xiaonan <xiaonan@innbit.com>
2017-11-05 15:45:11 +0800

    Replace cover image with carousel

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,16 +11,73 @@
 @endsection
 
 @section ('content')
-{{-- top area HTML --}}
-<div class="">
+{{-- Slider with Products + Solutions --}}
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="/img/home/cover-room.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Room</h3>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="/img/home/cover-curtain.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Curtain</h3>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="/img/home/cover-tv.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Television</h3>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="/img/home/cover-light.jpg" alt="Fourth slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Light</h3>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="/img/home/cover-music.jpg" alt="Fifth slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Music</h3>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="/img/home/cover-rs.jpg" alt="Sixth slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>Room Service</h3>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<!-- <div class="">
     <div class="card text-white">
-        <img src="/img/homepage/img_hotel.jpg" alt="" class="img-fluid card-img">
+        <img src="/img/home/img_hotel.jpg" alt="" class="img-fluid card-img">
         <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
             <h1 class="card-text txt_slogan">Make Your Space Alive</h1> 
             <button type="" class="btn btn-primary btn_learnmore">Learn More</button>
         </div>
     </div>
-</div>
+</div> -->
 
 
 {{-- products area HTML --}}
@@ -42,7 +99,7 @@
 {{-- SystemIntegration area HTML --}}
 <div class="m-b-30">
     <div class="card-xl text-white">
-        <img src="/img/homepage/img_system.jpg" class="card-img img_system" alt="Card image">
+        <img src="/img/home/img_system.jpg" class="card-img img_system" alt="Card image">
         <div class="d-flex flex-column justify-content-center align-items-start card-img-overlay-xl width-25 card-img-overlay-sm color-black-sm m-l-200">
             <h3 class="txt_integration_title">System Integration</h3>
             <p class="General-introduction">General introduction info about system integration functionality</p>
@@ -66,7 +123,7 @@
 {{-- fullPicture area HTML --}}
 <div class="m-b-30">
     <div class="card text-white">
-        <img src="/img/homepage/zerowire.jpg" alt="" class="img-fluid card-img">
+        <img src="/img/home/zerowire.jpg" alt="" class="img-fluid card-img">
         <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
             <h1 class="card-text ZERO-DOWN-ZERO-WIRE">ZERO DOWN</h1>
             <h1 class="card-text ZERO-DOWN-ZERO-WIRE">ZERO WIRE</h1>
@@ -81,21 +138,21 @@
         <h2 class="number">$ 123,456,789</h2>
     </div>
     <div class="d-flex flex-wrap justify-content-around align-items-center w-50">
-        <img src="/img/homepage/logo_1.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_2.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_3.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_4.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_5.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_1.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_2.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_3.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_4.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_5.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_1.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_2.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_3.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_4.png" alt=""  class="client_logo rounded-circle">
-        <img src="/img/homepage/logo_5.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_1.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_2.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_3.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_4.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_5.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_1.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_2.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_3.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_4.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_5.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_1.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_2.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_3.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_4.png" alt=""  class="client_logo rounded-circle">
+        <img src="/img/home/logo_5.png" alt=""  class="client_logo rounded-circle">
     </div>
 </div>
 

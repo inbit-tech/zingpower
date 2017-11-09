0c03d8e364c7273030403ae6029f634175f88a64
Xiaonan Cui <xiaonan@innbit.com>
2017-11-09 13:37:15 +0800

    Fix header center alignment issue

--- resources/views/layout/nav/header.blade.php
+++ resources/views/layout/nav/header.blade.php
@@ -1,12 +1,15 @@
 <nav class="navbar fixed-top navbar-expand-xl navbar-dark">
-	<a class="navbar-brand" href="/">
+	{{-- <a class="navbar-brand" href="/">
 		<img id="zp-logo" src=" {{ asset('/img/zp-logo-white.png') }} " height="50px" alt="">
-	</a>
+	</a> --}}
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-	<div class="collapse navbar-collapse justify-content-left" id="navbarNavDropdown">
+	    <span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse justify-content-left justify-content-center" id="navbarNavDropdown">
 		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="/news">Zing Power</a>
+			</li>
 			<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 					Products

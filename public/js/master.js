36b088b1941d5de825210db935fc3599fef04da8
Xiaonan Cui <xiaonan@innbit.com>
2017-11-17 15:12:22 +0800

    Rearrange header style

--- public/js/master.js
+++ public/js/master.js
@@ -1,15 +1,15 @@
 $(window).bind('scroll', function() {
-	if ($(window).scrollTop() > 50) {
-		$('.navbar').removeClass('navbar-dark');
-		$('.navbar').addClass('bg-light navbar-light');
-		$('#zp-logo').attr({
-			'src': '/img/zp-logo-color.png'
-		});
+	if ($(window).scrollTop() > 100) {
+		// $('.navbar').removeClass('navbar-dark');
+		// $('.navbar').addClass('bg-light navbar-light');
+		// $('#zp-logo').attr({
+		// 	'src': '/img/zp-logo-color.png'
+		// });
 	} else {
-		$('.navbar').removeClass('bg-light navbar-light');
-		$('.navbar').addClass('navbar-dark');
-		$('#zp-logo').attr({
-			'src': '/img/zp-logo-white.png'
-		});
+		// $('.navbar').removeClass('bg-light navbar-light');
+		// $('.navbar').addClass('navbar-dark');
+		// $('#zp-logo').attr({
+		// 	'src': '/img/zp-logo-white.png'
+		// });
 	}
 });
--- resources/views/layout/nav/header.blade.php
+++ resources/views/layout/nav/header.blade.php
@@ -1,38 +1,45 @@
-<nav class="navbar fixed-top navbar-expand-xl navbar-dark">
-	{{-- <a class="navbar-brand" href="/">
-		<img id="zp-logo" src=" {{ asset('/img/zp-logo-white.png') }} " height="50px" alt="">
-	</a> --}}
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-	    <span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse justify-content-left justify-content-center" id="navbarNavDropdown">
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="/">Zing Power</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Products
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-		        	<a class="dropdown-item" href="/products/walkie-talkie">Walkie Talkie</a>
-		        	<a class="dropdown-item" href="/products/wirelss-charger">Wireless Charger</a>
-		        	<a class="dropdown-item" href="/products/smart-room-controller">Smart Room App</a>
-		        </div>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#"  id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Solutions
-				</a>
-				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-		         	<a class="dropdown-item" href="/solutions/system-integration">System Integration</a>
-		        	<a class="dropdown-item" href="/solutions/dms">Device Management System</a>
-		        	<a class="dropdown-item" href="/solutions/lns">IoT</a>
-		        </div>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="/contact">Contact Us</a>
-			</li>
-		</ul>
+<nav class="navbar fixed-top navbar-light navbar-expand-xl zp-navi-large" id="zp-navi">
+	<div class="zp-navi-inner">
+		<div class="zp-navi-item">
+			<a class="navbar-brand" href="/">
+				<img id="zp-logo" src=" {{ asset('/img/zp-logo-color.png') }} " alt="">
+			</a>
+		</div>
+		
+		{{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button> --}}
+		<div class="zp-navi-item">
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Products
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				        	<a class="dropdown-item" href="/products/walkie-talkie">Walkie Talkie</a>
+				        	<a class="dropdown-item" href="/products/wirelss-charger">Wireless Charger</a>
+				        	<a class="dropdown-item" href="/products/smart-room-controller">Smart Room App</a>
+				        </div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#"  id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Solutions
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				         	<a class="dropdown-item" href="/solutions/system-integration">System Integration</a>
+				        	<a class="dropdown-item" href="/solutions/dms">Device Management System</a>
+				        	<a class="dropdown-item" href="/solutions/lns">IoT</a>
+				        </div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/about-us">About Us</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/contact">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </nav>

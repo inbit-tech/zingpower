a6b89445f67e7e57e555075fc1167af49f10f6e3
Xiaonan Cui <xiaonan@innbit.com>
2017-11-23 03:16:59 +0800

    Update 404 Page with laravel default page style

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Page not found - Zing Power</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" type="text/css" href="/css/pages/404.css">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Something wrong
+                </div>
+
+                <div class="links">
+                    <a href="/"> <<< Back to Home </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

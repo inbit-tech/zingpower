86e2c31195457084cebdd9cf1830a6682ffe7850
vayne <yaoxi@innbit.com>
2017-12-01 02:49:42 +0800

    complete Room Controller Interactive Module all click and hover events.Ensure the integrity and reversibility of the interaction process

--- public/js/pages/room-ctrl-module.js
+++ public/js/pages/room-ctrl-module.js
@@ -2,8 +2,9 @@
  * @Author: vayne
  * @Date:   2017-11-24 16:56:17
  * @Last Modified by:   vayne
- * @Last Modified time: 2017-11-30 16:12:40
+ * @Last Modified time: 2017-12-01 02:46:41
  */
+
 // Move the mouse, zoom in and out, and change the transparency of the ring
 $('#room-ctrl-phone-original-container').hover(function() {
     $('#room-ctrl-icon-finger').addClass('animated pulse');
@@ -12,64 +13,155 @@ $('#room-ctrl-phone-original-container').hover(function() {
     $('#room-ctrl-icon-finger').removeClass('animated pulse');
 });
 
-// Click on the finger, the background picture switch, the mobile phone content pictures change
+// Click on the finger, inter hotel-services and room-services phone container
 $('#room-ctrl-icon-finger').click(function() {
-    $('#room-ctrl-phone-original-container').detach();
-    $('#room-ctrl-rolling').detach();
-    $('#room-ctrl-icon-finger').detach();
-    $('#room-ctrl-tip-text').detach();
-    $('#room-ctrl-original-img').attr("src", "/img/room-ctrl/monochrome.jpg");
-    $('#room-ctrl-logo').attr("src", "/img/room-ctrl/logo-green.png");
-    $('#room-ctrl-phone-container').append('<img src="/img/room-ctrl/phone/phone-bg.png" alt="" id="room-ctrl-phone-bg">');
-    $('#room-ctrl-phone-container').append('<img src="/img/room-ctrl/phone/phone-ctrl-icon-gray.png" alt="" id="room-ctrl-phone-ctrl-option">');
-    $('#room-ctrl-phone-container').append('<img src="/img/room-ctrl/phone/phone-ctrl-hotel-ser-origin.png" alt="" id="room-ctrl-phone-ctrl-hotel-ser">');
-    $('#room-ctrl-phone-container').append('<img src="/img/room-ctrl/phone/phone-ctrl-room-ser-origin.png" alt="" id="room-ctrl-phone-ctrl-room-ser">');
-    $('#room-ctrl-phone-container').append('<img src="/img/room-ctrl/phone/phone-ctrl-open-btn.png" alt="" id="room-ctrl-phone-ctrl-open-btn">');
-    $('.room-ctrl-container').append('<img src="/img/room-ctrl/room-ctrl-hotel-ser.png" alt="" id="room-ctrl-hotel-ser" style="display: none">')
+    $('#room-ctrl-original-img').attr("src","/img/room-ctrl/monochrome.jpg")
+    $('#room-ctrl-phone-container-hotel-ser').css("display", "");
+    $('#room-ctrl-phone-original-container').css("display", "none");
+
+    // hover and click switch button event
+    $('#room-ctrl-phone-ctrl-open-btn').hover(function(){
+        $(this).attr("src","/img/room-ctrl/phone/phone-ctrl-open-btn.png");
+    },function(){
+        $(this).attr("src","/img/room-ctrl/phone/phone-ctrl-close-btn.png");
+    });
+    $('#room-ctrl-phone-ctrl-open-btn').click(function(){
+        // code complete
+    })
 
     // HOTEL SERVICES
-    // hover effect
+    // hover event
     $('#room-ctrl-phone-ctrl-hotel-ser').hover(function() {
         $(this).attr("src", "/img/room-ctrl/phone/phone-ctrl-hotel-ser.png");
-    },function(){
+    }, function() {
         $(this).attr("src", "/img/room-ctrl/phone/phone-ctrl-hotel-ser-origin.png");
     })
-    // HOTEL SERVICES click effect
-    $('#room-ctrl-phone-ctrl-hotel-ser').click(function(){
-        $('#room-ctrl-hotel-ser').toggle('slow');
+    // HOTEL SERVICES click event
+    $('#room-ctrl-phone-ctrl-hotel-ser').click(function() {
+        $('#room-ctrl-hotel-ser').toggle();
     });
 
-    
     // ROOM SERVICES
-    // hover effect
+    // hover event
     $('#room-ctrl-phone-ctrl-room-ser').hover(function() {
         $(this).attr("src", "/img/room-ctrl/phone/phone-ctrl-room-ser.png");
-    },function(){
+    }, function() {
         $(this).attr("src", "/img/room-ctrl/phone/phone-ctrl-room-ser-origin.png")
     })
 
-
-    // ROOM SERVICES click effect room-control Interface appear
-    $('#room-ctrl-phone-ctrl-room-ser').click(function(){
-        $('#room-ctrl-hotel-ser').css("display","none");
-        $('#room-ctrl-phone-container').detach();
-        $('#room-ctrl-phone-container-room-ser').toggle('slow');
-    })
-
-    // click icon-air effect
-    $('#phone-room-ctrl-icon-air').click(function(){
-        $("#icon-air-click-after-img").toggle('slow');
-    })
-
-    // click icon-tv effect
-    $('#phone-room-ctrl-icon-tv').click(function(){
-        $("#icon-tv-click-after-img").toggle('slow');
-    })
-
-    // click icon-curtain effect
-    $('#phone-room-ctrl-icon-curtain').click(function(){
-        $("#icon-curtain-click-after-img").toggle('slow');
+    // ROOM SERVICES click event room-control Interface appear
+    $('#room-ctrl-phone-ctrl-room-ser').click(function() {
+        $('#room-ctrl-phone-container-hotel-ser').css("display", "none");
+        $('#room-ctrl-phone-container-room-ctrl').css("display", "");;
     })
 })
 
 
+
+// room control phone container each icon hover and click event
+// room-ctrl-icon-callback click event
+$('#room-ctrl-icon-callback').click(function() {
+    $('#room-ctrl-phone-container-hotel-ser').css("display", "");
+    $('#room-ctrl-phone-container-light-ctrl').css("display", "none");
+    // when click room-ctrl-icon-callback ,
+    // that init hotel-services and room-services phone container
+    $('#room-ctrl-hotel-ser').css("display", "none");
+    $('#icon-air-click-after-img').css("display", "none");
+    $('#icon-tv-click-after-img').css("display", "none");
+    $('#icon-curtain-click-after-img').css("display", "none");
+
+})
+// icon-air hover event
+$('#phone-room-ctrl-icon-air').hover(function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-air-after.png");
+}, function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-air.png");
+})
+// click icon-air event
+$('#phone-room-ctrl-icon-air').click(function() {
+    $('#icon-air-click-after-img').toggle();
+})
+
+// icon-tv hover event
+$('#phone-room-ctrl-icon-tv').hover(function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-tv-after.png");
+}, function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-tv.png");
+})
+// click icon-tv event
+$('#phone-room-ctrl-icon-tv').click(function() {
+    $('#icon-tv-click-after-img').toggle();
+})
+
+// icon-curtain hover event
+$('#phone-room-ctrl-icon-curtain').hover(function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-curtain-after.png");
+}, function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-curtain.png");
+})
+// click icon-curtain event
+$('#phone-room-ctrl-icon-curtain').click(function() {
+    $('#icon-curtain-click-after-img').toggle();
+})
+
+// icon-light hover event
+$('#phone-room-ctrl-icon-light').hover(function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-light-after.png");
+}, function() {
+    $(this).attr('src', "/img/room-ctrl/phone/phone-room-ctrl-icon-light.png");
+})
+// click icon-light event
+$('#phone-room-ctrl-icon-light').click(function() {
+    $('#room-ctrl-phone-container-light-ctrl').css("display", "");
+    $('#room-ctrl-phone-container-room-ctrl').css("display", "none");
+})
+
+
+
+
+
+
+// light control phone container each icon hover and click event
+// light-ctrl-icon-callback click event
+$('#light-ctrl-icon-callback').click(function() {
+    $('#room-ctrl-phone-container-room-ctrl').css("display", "");
+    $('#room-ctrl-phone-container-light-ctrl').css("display", "none");
+    // when click light-ctrl-icon-callback ,
+    // that init room control  phone container
+    $('#icon-bed-light-click-after-img').css("display", "none");
+    $('#icon-main-light-click-after-img').css("display", "none");
+    $('#icon-office-light-click-after-img').css("display", "none");
+})
+
+// icon-bed-light hover event
+$('#bed-light').hover(function() {
+    $(this).attr("src", "img/room-ctrl/phone/light-ctrl/bed-light-after.png");
+}, function() {
+    $(this).attr("src", "img/room-ctrl/phone/light-ctrl/bed-light.png");
+});
+// icon-bed-light click event
+$('#bed-light').click(function() {
+    $('#icon-bed-light-click-after-img').toggle();
+});
+
+// icon-main-light hover event
+$('#main-light').hover(function() {
+    $(this).attr("src", "img/room-ctrl/phone/light-ctrl/main-light-after.png");
+}, function() {
+    $(this).attr("src", "img/room-ctrl/phone/light-ctrl/main-light.png");
+});
+// icon-main-light click event
+$('#main-light').click(function() {
+    $('#icon-main-light-click-after-img').toggle();
+});
+
+// icon-office-light hover event
+$('#office-light').hover(function() {
+    $(this).attr("src", "img/room-ctrl/phone/light-ctrl/office-light-after.png");
+}, function() {
+    $(this).attr("src", "img/room-ctrl/phone/light-ctrl/office-light.png");
+});
+// icon-office-light click event
+$('#office-light').click(function() {
+    $('#icon-office-light-click-after-img').toggle();
+});
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -18,17 +18,37 @@
 <section id="cover-wrapper" class="d-flex justify-content-center">
     <div class="room-ctrl-container">
         <img src="/img/room-ctrl/original.jpg" alt="" id="room-ctrl-original-img">
-        {{--  click room services after this change corresponding img display --}}
+
+        {{--  in room-control phone container click each icon after this change corresponding img display --}}
         <img src="/img/room-ctrl/room-ctrl-air-wind.png" id="icon-air-click-after-img" class="icon-click-after-img" style="display: none;">
         <img src="/img/room-ctrl/room-ctrl-tv-light.png" id="icon-tv-click-after-img" class="icon-click-after-img" style="display: none;">
         <img src="/img/room-ctrl/room-ctrl-curtain-open.png" id="icon-curtain-click-after-img" class="icon-click-after-img" style="display: none;">
 
+        {{-- in light-control phone container click each icon after this change corresponding img display --}}
+        <img src="/img/room-ctrl/room-ctrl-bed-light-open.png" id="icon-bed-light-click-after-img" class="icon-click-after-img" style="display: none;">
+        <img src="/img/room-ctrl/room-ctrl-main-light-open.png" id="icon-main-light-click-after-img" class="icon-click-after-img" style="display: none;">
+        <img src="/img/room-ctrl/room-ctrl-office-light-open.png" id="icon-office-light-click-after-img" class="icon-click-after-img" style="display: none;">
+
         <img src="/img/room-ctrl/logo.png" alt="" id="room-ctrl-logo">
         <img src="/img/room-ctrl/phone.png" alt="" id="room-ctrl-phone">
 
-        {{-- click room services after phone container --}}
-        <div id="room-ctrl-phone-container-room-ser" style="display: none;">
+        {{-- light control  phone container --}}
+        <div id="room-ctrl-phone-container-light-ctrl" style="display: none;">
             <img src="/img/room-ctrl/phone/phone-bg.png" alt="" id="room-ctrl-phone-bg">
+            <img src="img/room-ctrl/phone/light-ctrl/callback.png" alt="" id="light-ctrl-icon-callback">
+            <p id="light-ctrl-text">LIGHT CONTROL</p>
+            <img src="img/room-ctrl/phone/light-ctrl/bed-light.png" alt="" id="bed-light">
+            <img src="img/room-ctrl/phone/light-ctrl/main-light.png" alt="" id="main-light">
+            <img src="img/room-ctrl/phone/light-ctrl/bathroom-light.png" alt="" id="bathroom-light">
+            <img src="img/room-ctrl/phone/light-ctrl/office-light.png" alt="" id="office-light">
+            <img src="img/room-ctrl/phone/light-ctrl/ground-light.png" alt="" id="ground-light">
+            <img src="img/room-ctrl/phone/light-ctrl/corridor-light.png" alt="" id="corridor-light">
+        </div>
+
+        {{-- room control  phone container --}}
+        <div id="room-ctrl-phone-container-room-ctrl" style="display: none;">
+            <img src="/img/room-ctrl/phone/phone-bg.png" alt="" id="room-ctrl-phone-bg">
+            <img src="img/room-ctrl/phone/light-ctrl/callback.png" alt="" id="room-ctrl-icon-callback">
             <p id="room-ser-text">ROOM CONTROL</p>
             <img src="/img/room-ctrl/phone/phone-room-ctrl-icon.png" alt="" id="phone-room-ctrl-icon">
             <img src="/img/room-ctrl/phone/phone-room-ctrl-icon-air.png" alt="" id="phone-room-ctrl-icon-air">
@@ -37,12 +57,18 @@
             <img src="/img/room-ctrl/phone/phone-room-ctrl-icon-curtain.png" alt="" id="phone-room-ctrl-icon-curtain">
         </div>
 
-        {{-- click icon-finger after phone container --}}
-        <div id="room-ctrl-phone-container">
+        {{-- hotel-services and room-services phone container --}}
+        <div id="room-ctrl-phone-container-hotel-ser" style="display: none;">
+            <img src="/img/room-ctrl/phone/phone-bg.png" alt="" id="room-ctrl-phone-bg">
+            <img src="/img/room-ctrl/phone/phone-ctrl-icon-gray.png" alt="" id="room-ctrl-phone-ctrl-option">
+            <img src="/img/room-ctrl/phone/phone-ctrl-hotel-ser-origin.png" alt="" id="room-ctrl-phone-ctrl-hotel-ser">
+            <img src="/img/room-ctrl/phone/phone-ctrl-room-ser-origin.png" alt="" id="room-ctrl-phone-ctrl-room-ser">
+            <img src="/img/room-ctrl/phone/phone-ctrl-close-btn.png" alt="" id="room-ctrl-phone-ctrl-open-btn">
+            <img src="/img/room-ctrl/room-ctrl-hotel-ser.png" alt="" id="room-ctrl-hotel-ser" style="display: none">
         </div> 
 
         {{-- original phone container --}}
-        <div id="room-ctrl-phone-original-container">
+        <div id="room-ctrl-phone-original-container" style="display:">
             <img src="/img/room-ctrl/rolling.png" alt="" id="room-ctrl-rolling" class="opacity-20">
             <img src="/img/room-ctrl/icon-finger.png" alt="" id="room-ctrl-icon-finger">
             <p id="room-ctrl-tip-text">click to control the room</p>            

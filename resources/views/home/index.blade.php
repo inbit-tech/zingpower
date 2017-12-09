ffe4ebcc30cd9bf9c318514cc9bdfcdc7a674c23
xiaonan <xiaonan@innbit.com>
2017-12-10 01:39:08 +0800

    Replace product section with 3 cards

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -78,7 +78,29 @@
 {{-- products area HTML --}}
 
 <div class="products-wrapper row mx-auto">
-    
+    <div class="card-deck">
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>
 </div>
 
 {{-- SystemIntegration area HTML --}}

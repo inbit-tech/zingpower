d1e474d6e34805012ea5583306544993df7fa945
xiaonan <xiaonan@innbit.com>
2017-12-12 01:45:56 +0800

    Clear previous walkie talkie codes

--- resources/views/products/wata.blade.php
+++ resources/views/products/wata.blade.php
@@ -11,59 +11,5 @@
 @endsection
 
 @section ('content')
-{{-- hero picture --}}
-<div class="m-b-30">
-    <div class="card text-white">
-        <img src="/img/products/walkietalkie.jpeg" alt="" class="img-fluid card-img">
-        <div class="card-img-overlay d-flex justify-content-center align-items-center">
-            <h1 class="Walkie-Talkie">Walkie Talkie</h1>
-        </div>
-    </div>
-</div>
 
-{{-- text --}}
-<div class="m-b-30 p-5 d-flex flex-wrap justify-content-center">
-    <p  class="w-50">Our radios are suitable for use in shopping malls, schools, colleges, hotels, bars & pubs, nightclubs, door supervisors, farms, garden centers, warehouses, factories, general security work, and for outdoor activities including skiing, paintball, cycling, running etc.</p>
-</div>
-
-{{-- carousel --}}
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100 img_slide" src="/img/products/slide1.jpeg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100 img_slide" src="/img/products/slide2.jpeg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100 img_slide" src="/img/products/slide3.jpeg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-{{-- img item --}}
-<div class="m-b-30 mt-5 d-flex flex-wrap justify-content-center">
-    <div class="p-4 d-flex justify-content-between flex-column align-items-center">
-        <img src="http://placeimg.com/200/200/animals" alt="" class="rounded-circle">
-        <p class="Radios mt-3">Radios</p>
-    </div>
-
-    <div class="p-4 d-flex justify-content-between flex-column align-items-center">
-        <img src="http://placeimg.com/200/200/nature" alt="" class="rounded-circle">
-        <p class="Accessories mt-3">Accessories</p>
-    </div>
-
-    <div class="p-4 d-flex justify-content-between flex-column align-items-center">
-        <img src="http://placeimg.com/200/200/architecture" alt="" class="rounded-circle">
-        <p class="-Way-Radios mt-3">2-Way Radios</p>
-    </div>
-</div>
 @endsection

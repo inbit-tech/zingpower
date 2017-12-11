809cf0cfcd79f4b99461cdd8d557c95f3541ec86
xiaonan <xiaonan@innbit.com>
2017-12-12 04:55:35 +0800

    Complete walkie talkie page fix

--- resources/views/products/wata.blade.php
+++ resources/views/products/wata.blade.php
@@ -11,5 +11,29 @@
 @endsection
 
 @section ('content')
+<!-- Cover Image -->
+<section id="cover-wrapper">
+  <div class="cover-image">
+    <p class="cover-text">Connect and Talk</p>
+  </div>
+</section>
+
+<!-- Content Link -->
+<section id="wata-wrapper">
+  <div class="walkie-talkie mx-auto">
+    <div class="wata-bg-wrapper">
+      <h1>The capacity to increase your scope</h1>
+      <div class="wata-bg-1"></div>
+    </div>
+    <div class="wata-bg-wrapper">
+      <h1>There is a story in every detail</h1>
+      <div class="wata-bg-2"></div>
+    </div>
+    <div class="wata-bg-wrapper">
+      <h1>Help you save time to get more power</h1>
+      <div class="wata-bg-3"></div>
+    </div>
+  </div>
+</section>
 
 @endsection

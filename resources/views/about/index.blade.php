5560bfbecaf9d8b7eb75a6f25906b227fb7507e2
Xiaonan Cui <xiaonan@innbit.com>
2017-12-25 02:24:41 +0800

    Restyle text n title with image'

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -20,9 +20,9 @@
 </section>
 <section id="content-wrapper">
 	<div class="whoweare">
-		<div class="whoweare-title">
+		<p class="about-title whoweare-title">
 			Who We Are
-		</div>
+		</p>
 		<div class="whoweare-content">
 			<p>We are a R&D house based in Xi’an China, with a team of electrical engineers and specialists, designing, developing and customizing in electrical and semi-conductor related products, to meet customers’ expectations and unique requirement. Another team of software developers specialized in mobile technologies and business analytics.</p>
 			<p>We have provided extensive mobile phone and wireless charging solutions to over 120 customer bases in various industries mainly in F&B, Logistics, Automobile,hospitality, educations and etc. </p>
@@ -30,18 +30,18 @@
 	</div>
 	<div class="vision">
 		<div class="vision-image">
-			<h1 class="vision-title">
+			<p class="about-title vision-title">
 				Our Vision
-			</h1>
+			</p>
 			<p class="vision-text">
 				An endless possibilities where not only everyone but also everything gets connected for better style of living.
 			</p>
 		</div>
 	</div>
 	<div class="whatwedo">
-		<div class="whatwedo-title">
+		<p class="about-title whatwedo-title">
 			What We Do
-		</div>
+		</p>
 		<div class="whatwedo-content">
 			<p>We deliver a complete and comprehensive working solution to help hotels to achieveseamless operations and excellence in their services, cost reduction and simultaneously drive their revenue growth.</p>
 			<p>Our exclusive solution includes combinations of smart devices, apps, backendsystem, database and etc., it is customizable to match each customers' unique requirements.</p>
@@ -49,10 +49,10 @@
 	</div>
 	<div class="mission">
 		<div class="mission-image">
-			<h1 class="mission-title">
+			<p class="about-title mission-title">
 				Our Mission
-			</h1>
-			<p class="mission-text">
+			</p>
+			<p class="about-text mission-text">
 				To integrate SMART solution for in-room services using quality products with innovative technology.
 			</p>
 		</div>
